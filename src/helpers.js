b1cbbfcfd12a16e50405244b752fd89a523cb283
BossBele <adambeleko@gmail.com>
2021-12-27 13:52:28 +0300

    add web support

--- src/helpers.js
+++ src/helpers.js
@@ -3,9 +3,11 @@
 import { Platform, Dimensions } from 'react-native';
 
 const Screen = Dimensions.get('window');
-export const ScreenWidth: number = Screen.width;
-export const ScreenHeight: number = Screen.height;
+export const ScreenWidth = Screen.width;
+export const ScreenHeight = Screen.height;
 export const isIOS = Platform.OS === 'ios';
+export const isAndroid = Platform.OS === 'android';
+export const isWeb = Platform.OS === 'web';
 
 export const Colors = {
   darkergray: '#617080',
